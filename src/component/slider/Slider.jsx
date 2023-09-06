68590456870787cbec0475d33c6c204835de0a54
cmunoz <cmunoz@idrall.com>
2023-09-06 15:48:49 -0600

    Transition effect

--- src/component/slider/Slider.jsx
+++ src/component/slider/Slider.jsx
@@ -13,21 +13,29 @@ var file = [
 const Slider = () => {
     const [data, setData] = useState(file);
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [selectedItem, setSelectedItem] = useState(null);
 
-    const handleNext = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+
+    const handleNext = (item) => {
+        if(currentIndex <= data.length -2){
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+            setSelectedItem(item);
+        }
+        console.log(data.length)
+        console.log(currentIndex)
     };
 
     const handlePrev = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + data.length) % data.length);
-        console.log(currentIndex)
+        if(currentIndex -1 >= 0){
+            setCurrentIndex((prevIndex) => (prevIndex - 1 ) % data.length);
+        }
     };
 
     return (
         <div className="container mt-4">
             <div className="row">
                 <div className="col-12 text-center mb-3">
-                    <button onClick={handlePrev} className="btn btn-primary">Anterior</button>
+                    <button onClick={handlePrev} className={`btn btn-primary ${currentIndex === 0 ? 'inactiveFull' : ''}`}>Anterior</button>
                     {/*<button onClick={handleNext} className="btn btn-primary ml-2">Siguiente</button>*/}
                 </div>
             </div>
@@ -42,11 +50,11 @@ const Slider = () => {
                                 <h5 className={`card-title ${index === currentIndex ? '' : 'inactiveFull'}`}>Tabla {item.id}</h5>
                                 <table className="table">
                                     <tbody>
-                                    {data.map((item) => (
+                                    {data.map((item, index) => (
                                         <tr
-                                            className={`card-title ${index === currentIndex ? '' : 'inactiveFull' && item.selected ? '' : 'inactiveFull'}`}
+                                            className={`card-title`}
                                             key={item.id}>
-                                            <td onClick={handleNext}>{item.content}</td>
+                                            <td onClick={()=> handleNext(item)}>{item.content}</td>
                                         </tr>
                                     ))}
                                     </tbody>
